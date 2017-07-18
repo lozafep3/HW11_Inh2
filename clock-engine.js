3a5a6186bf12e2c717487010840de94e62fad402
Arthur Loza <loza.uns@gmail.com>
2017-07-18 15:42:05 +0300

    HW11_Inh2: base clock done

---
+++ clock-engine.js
@@ -0,0 +1,57 @@
+function Clock() {
+    this.cHours = function() {
+        var cHours = new Date().getHours();
+
+        if (cHours < 10) {
+            cHours = '0' + cHours;
+        }
+
+        return cHours;
+    };
+
+    this.cMinutes = function() {
+        var cMinutes = new Date().getMinutes();
+
+        if (cMinutes < 10) {
+            cMinutes = '0' + cMinutes;
+        }
+
+        return cMinutes;
+    };
+
+    this.cSeconds = function() {
+        var cSeconds = new Date().getSeconds();
+
+        if (cSeconds < 10) {
+            cSeconds = '0' + cSeconds;
+        }
+
+        return cSeconds;
+    };
+}
+
+function ClockFull(id, timezone) {
+    Clock.call(this, arguments);
+
+    this.draw = function () {
+        document.getElementById(id).innerHTML = (+this.cHours() + timezone) + ':' + this.cMinutes() + ':' + this.cSeconds();
+    }
+}
+
+function ClockShort(id, timezone) {
+    Clock.call(this, arguments);
+
+    this.draw = function () {
+        document.getElementById(id).innerHTML = (+this.cHours() + timezone) + ':' + this.cMinutes();
+    }
+}
+
+//======================================================================
+
+setInterval(function () {
+    clockLocal.draw();
+    clockUTC.draw();
+}, 500);
+
+var clockLocal = new ClockFull('time_local', 0, true);
+var clockUTC = new ClockShort('time_utc', -2, true);
